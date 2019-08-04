8134581773f106bc4e9bdb19f1c226f5b476b7b2
unknown <halfcounter55@hotmail.com>
2019-08-04 16:26:29 -0700

    Uploading my RTC code to git Y1Q1

---
+++ ConsoleApplication1/Excersize001.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // This is the start of your program
+        // This is the entry point to your program
+        // This is the main method
+
+        static void topOfASCII() // Prints the top of the ASCII STOP sign
+        {
+            Console.WriteLine(" _____  ");
+            Console.WriteLine(" /     \\ ");
+            Console.WriteLine("/       \\");
+        }
+
+        static void bottomOfASCII() // Prints the bottom of the ASCII STOP sign
+        {
+            Console.WriteLine("\\       /");
+            Console.WriteLine(" \\_____/ ");
+        }
+
+        static void plusASCII()
+        {
+            Console.WriteLine("+-------+"); // Prints the + and - characters for the very bottom of the ASCII stop sign
+        }
+
+        static void stopASCII()
+        {
+            Console.WriteLine("|  STOP |"); // Prings the STOP and sides of the ASCII STOP sign
+        }
+
+        static void Main(string[] args) // Main method that requires 3 inputs to get all of the ASCII art, 1 sign at a time
+        { // body
+            Console.ReadKey();
+            topOfASCII();
+            bottomOfASCII();
+            Console.ReadKey();
+
+            topOfASCII();
+            bottomOfASCII();
+            plusASCII();
+            Console.ReadKey();
+
+            topOfASCII();
+            stopASCII();
+            bottomOfASCII();
+            plusASCII();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/MyFirstProject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // This is the start of your program
+        // This is the entry point to your program
+        // This is the main method
+        static void Main(string[] args)
+        { // body
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+
+            Console.WriteLine(" _____  ");
+            Console.WriteLine(" /     \\ ");
+            Console.WriteLine("/       \\");
+            Console.WriteLine("\\       /");
+            Console.WriteLine(" \\_____/ ");
+            Console.ReadKey();
+
+            Console.WriteLine(" _____  ");
+            Console.WriteLine(" /     \\ ");
+            Console.WriteLine("/       \\");
+            Console.WriteLine("\\       /");
+            Console.WriteLine(" \\_____/ ");
+            Console.WriteLine("+-------+");
+            Console.ReadKey();
+
+            Console.WriteLine(" _____  ");
+            Console.WriteLine(" /     \\ ");
+            Console.WriteLine("/       \\");
+            Console.WriteLine("|  STOP |");
+            Console.WriteLine("\\       /");
+            Console.WriteLine(" \\_____/ ");
+            Console.WriteLine("+-------+|");
+            Console.ReadKey();
+        }
+    }
+}
